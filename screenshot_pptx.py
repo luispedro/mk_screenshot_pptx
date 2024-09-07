eeb749edab9de4739c332b83f96bfa8aa488be68
Luis Pedro Coelho <luis@luispedro.org>
2024-09-08 09:18:06 +1000

    ENH Add some command line options

--- screenshot_pptx.py
+++ screenshot_pptx.py
@@ -8,9 +8,9 @@ import tempfile
 # Key to advance slides
 ADVANCE_SLIDE_KEY = 'J'
 # Output file name
-OUTPUT_NAME = 'Snowman.pptx'
+DEF_OUTPUT_NAME = 'Snowman.pptx'
 # Time to wait between slides (in seconds)
-SLIDE_DELAY = 1
+DEF_SLIDE_DELAY = 1
 
 def get_screenshot(fname):
     subprocess.check_call(['scrot', '--monitor', '0', fname])
@@ -21,10 +21,28 @@ def advance_slide():
     subprocess.check_call(['xdotool', 'key', ADVANCE_SLIDE_KEY])
 
 
+def parse_args(args):
+    import argparse
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                    description='Create PPTX from any presentation (by screenshotting)')
+    parser.add_argument('--delay',
+                        required=False,
+                        type=int,
+                        default=DEF_SLIDE_DELAY,
+                        action='store',
+                        dest='slide_delay')
+    parser.add_argument('-o', '--output',
+                        required=False,
+                        default=DEF_OUTPUT_NAME,
+                        dest='output_name')
+    return parser.parse_args(args)
+
+
 def main(args=None):
     if args is None:
         from sys import argv
-        args = argv
+        args = argv[1:]
+    opts = parse_args(args)
 
     print('Creating slides')
     print(f'Will simulate pressing {ADVANCE_SLIDE_KEY} to advance slides')
@@ -52,10 +70,10 @@ def main(args=None):
             prev = im
 
         advance_slide()
-        sleep(SLIDE_DELAY)
+        sleep(opts.slide_delay)
 
-    prs.save(OUTPUT_NAME)
-    print(f'Saved to {OUTPUT_NAME}')
+    prs.save(opts.output_name)
+    print(f'Saved to {opts.output_name}')
 
 if __name__ == '__main__':
     main()
